d5b3f208c224d8b9622d1731e2b37053a4d4edb3
Lizeth Contreras <lizethcontreras414@gmail.com>
2023-02-09 11:23:46 -0600

    Add project files.

---
+++ Archivo1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+StreamWriter fichero;
+string nombre = "tabla.txt";
+
+//fichero = fila.AppendText(nombre);//crear el archivo o  lo reutiliza 
+try
+{
+    StreamReader milectura= File.OpenText(nombre);//Abrio el archivo 
+    Console.WriteLine(milectura);
+
+}catch(Exception nsg)
+
+
+
